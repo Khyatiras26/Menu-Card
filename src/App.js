131db12ba2733fe15274db200fcf93461ee59ff7
Khyati <khyati2609@gmail.com>
2023-05-20 12:14:01 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import Restaurant from './component/Basics/Restaurant'
+
+
+
+
+
+
+const App = () => {
+  return (
+    <div>
+       <Restaurant/> 
+      
+      
+    </div>
+  )
+}
+
+
+
+export default App
